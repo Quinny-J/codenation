f13baf1e6aa091940c16284168f8043f58d40f3c
Quinny-J <93888757+Quinny-J@users.noreply.github.com>
2024-06-28 10:31:15 +0100

    Comments

--- Week 1/Day 2/variables.py
+++ Week 1/Day 2/variables.py
@@ -2,11 +2,11 @@
 print("All Around The World"[7].upper()) # Print the 8th char in uppercase. Always starts at 0 not 1 :)
 
 # Use and print a var
-# if your lazy make a var of a var instead of renaming  | mn = my_name
+# if your lazy make a var of a var instead of renaming  | mn = my_name not good practise at all 
 my_name = "Josh"
 print("Hello, " + my_name)
 
-# Take user input
+# Take user input and assign it a variable
 my_name = input("Please enter your name: ") # "Josh"
 my_age = input("Please enter your age: ") # "21"
 student = input("Are you a student Y/N: ".lower()) # "Y"
@@ -16,17 +16,16 @@ student = input("Are you a student Y/N: ".lower()) # "Y"
 print(f"Hello, {my_name}")
 print(f"You are {my_age} Years old")
 
+# another way of doing it
+print("Hello, " + my_name)
+print("You are " + my_age + " Years old")
+
 # Method 1
 print(f"Hello, {my_name} you are {my_age} Years old")
 # Method 2
 print("Hello, " + my_name + " you are " + my_age + " Years old")
 # Method 3 - Many easier ways to do this
 print("Hello, {} you are {} Years old".format(my_name, my_age))
-
-# 2 ways of doing it
-print("Hello, " + my_name)
-print("You are " + my_age + " Years old")
-
 
 # Check user input and deal with it
 if student == "y":
