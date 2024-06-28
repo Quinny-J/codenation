839f2aa30f6b76d9d69bff10e11236709da39ac0
Quinny-J <93888757+Quinny-J@users.noreply.github.com>
2024-06-28 12:24:14 +0100

    Added cur place holder

--- Week 1/Day 2/store.py
+++ Week 1/Day 2/store.py
@@ -12,8 +12,8 @@ def doAppleCost(val1, appleprice):
 
 # Take user input and assign variables
 apple_price = 0.25
-my_val1 = int(input("How many apples would you like to buy: ")) # "2"
+my_val1 = int(input(f"({apple_price}p per apple)How many apples would you like to buy: ")) # "2"
 total = doAppleCost(my_val1, apple_price)
 
 # Print out the response the function gave us
-print(f"Total Cost : {total:.2f}")
+print(f"Total Cost : £{total:.2f}")
