ee340cac2f682600caa581f6fa153470af8c4732
Quinny-J <93888757+Quinny-J@users.noreply.github.com>
2024-06-28 10:35:46 +0100

    Commented prev code

--- Week 1/Day 2/variables.py
+++ Week 1/Day 2/variables.py
@@ -3,8 +3,8 @@ print("All Around The World"[7].upper()) # Print the 8th char in uppercase. Alwa
 
 # Use and print a var
 # if your lazy make a var of a var instead of renaming  | mn = my_name not good practise at all 
-my_name = "Josh"
-print("Hello, " + my_name)
+#my_name = "Josh"
+#print("Hello, " + my_name)
 
 # Take user input and assign it a variable
 my_name = input("Please enter your name: ") # "Josh"
@@ -13,19 +13,19 @@ student = input("Are you a student Y/N: ".lower()) # "Y"
 
 # Crazy magic
 # fstrings can include expresions in curly brackets
-print(f"Hello, {my_name}")
-print(f"You are {my_age} Years old")
+#print(f"Hello, {my_name}")
+#print(f"You are {my_age} Years old")
 
 # another way of doing it
-print("Hello, " + my_name)
-print("You are " + my_age + " Years old")
+#print("Hello, " + my_name)
+#print("You are " + my_age + " Years old")
 
-# Method 1
+# Method 1 using fstrings
 print(f"Hello, {my_name} you are {my_age} Years old")
 # Method 2
-print("Hello, " + my_name + " you are " + my_age + " Years old")
+#print("Hello, " + my_name + " you are " + my_age + " Years old")
 # Method 3 - Many easier ways to do this
-print("Hello, {} you are {} Years old".format(my_name, my_age))
+#print("Hello, {} you are {} Years old".format(my_name, my_age))
 
 # Check user input and deal with it
 if student == "y":
