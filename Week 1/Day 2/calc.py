fe67f9c6594fdd7b4b5b7f7651ec7dfb216d9ca3
Quinny-J <93888757+Quinny-J@users.noreply.github.com>
2024-06-28 11:13:49 +0100

    Made seperate files for easy reading

---
+++ Week 1/Day 2/calc.py
@@ -0,0 +1,25 @@
+# Define some methods that the calc uses
+methods = ["sub", "mult", "div", "add"]
+
+# calculate(val1, cal2, method)
+def calculate(val1, val2, method):
+    # check if we have the method requested
+    if method in methods:
+        if method == "mult":
+            return int(val1) * int(val2)
+        elif method == "sub":
+            return int(val1) - int(val2)
+        elif method == "add":
+            return int(val1) + int(val2)
+        elif method == "div":
+            return int(val1) / int(val2)
+        else:
+            return "Method not found"
+
+# Take user input and assign variables
+my_val1 = input("Number 1: ") # "2"
+my_method = input("Method (sub/mult/div/add): ") # "mult"
+my_val2 = input("Number 2: ") # "2"
+
+# Print out the response the function gave us
+print("Your result is : " + calculate(my_val1, my_val2, my_method))
---
+++ Week 1/Day 2/who.py
@@ -0,0 +1,26 @@
+# Take user input and assign it a variable
+my_name = input("Please enter your name: ") # "Josh"
+my_age = input("Please enter your age: ") # "21"
+student = input("Are you a student Y/N: ".lower()) # "Y"
+
+# Crazy magic
+# fstrings can include expresions in curly brackets
+#print(f"Hello, {my_name}")
+#print(f"You are {my_age} Years old")
+
+# another way of doing it
+#print("Hello, " + my_name)
+#print("You are " + my_age + " Years old")
+
+# Method 1 using fstrings put the var inside {}
+print(f"Hello, {my_name} you are {my_age} Years old")
+# Method 2
+#print("Hello, " + my_name + " you are " + my_age + " Years old")
+# Method 3 - Many easier ways to do this
+#print("Hello, {} you are {} Years old".format(my_name, my_age))
+
+# Check user input and deal with it
+if student == "y":
+    print("You are currently studying")
+else:
+    print("You are not currently studying")
