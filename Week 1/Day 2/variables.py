160ed9f2608d4269941405016b4d8ece6839a7a6
Quinny-J <93888757+Quinny-J@users.noreply.github.com>
2024-06-28 10:26:10 +0100

    Mashing things together. Fstrings

--- Week 1/Day 2/variables.py
+++ Week 1/Day 2/variables.py
@@ -1,1 +1,31 @@
+# All Around The World - Print the 8th character
 print("All Around The World"[7].upper()) # Print the 8th char in uppercase. Always starts at 0 not 1 :)
+
+# Use and print a var
+# if your lazy make a var of a var instead of renaming  | mn = my_name
+my_name = "Josh"
+print("Hello, " + my_name)
+
+# Take user input
+my_name = input("Please enter your name: ") # "Josh"
+my_age = input("Please enter your age: ") # "21"
+student = input("Are you a student Y/N: ".lower()) # "Y"
+
+# Crazy magic
+# fstrings can include expresions in curly brackets
+print(f"Hello, {my_name}")
+print(f"You are {my_age} Years old")
+
+print(f"Hello, {my_name} you are {my_age} Years old")
+print("Hello, " + my_name + " you are " + my_age + " Years old")
+
+# 2 ways of doing it
+print("Hello, " + my_name)
+print("You are " + my_age + " Years old")
+
+
+# Check user input and deal with it
+if student == "y":
+    print(student + " You are currently studying")
+else:
+    print(student + " You are not currently studying")
